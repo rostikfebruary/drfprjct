b2593b7024561cb732f8427a924c5d855935bb20
rostikfebruary <lyutyirostik@gmail.com>
2025-03-19 23:54:46 +0200

    init drf cars project

--- cars/models.py
+++ cars/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class NewCar(models.Model):
     class Meta:
         db_table = 'cars'
-    brand = models.CharField(max_length=60)
+    brand = models.CharField(max_length=40)
     price = models.IntegerField(default=0)
     year = models.IntegerField(default=0)
     v = models.FloatField(default=0)
